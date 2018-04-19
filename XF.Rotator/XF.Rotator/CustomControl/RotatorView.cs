5d781e4a808c07e29ef02301b0fc9de4eb4c70d1
Karim Said <kms@crossworkers.com>
2018-04-19 11:52:18 +0200

    Add project files.

---
+++ XF.Rotator/XF.Rotator/CustomControl/RotatorView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XF.Rotator.CustomControl
+{
+    public class RotatorView : ContentView
+    {
+        public RotatorView()
+        {
+
+        }
+
+        public RotatorView(ContentView view)
+        {
+            Content = view.Content;
+        }
+        public string ID { get; set; }
+    }
+}
---
+++ XF.Rotator/XF.Rotator/Models/Pages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using XF.Rotator.CustomControl;
+
+namespace XF.Rotator.Models
+{
+    public static class Pages
+    {
+        public static List<RotatorView> ThosePages = new List<RotatorView>()
+        {
+            new RotatorView()
+            {
+                BackgroundColor = Color.Yellow
+            },
+            new RotatorView()
+            {
+                BackgroundColor = Color.Black
+            },
+            new RotatorView()
+            {
+                BackgroundColor = Color.Blue
+            }
+
+
+        };
+    }
+}
